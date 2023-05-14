0bb104a4e9db2843db88b4f7d0c6ecd38f61c66b
Justyn Mahen <justyn2s04mahen23@gmail.com>
2023-05-15 00:11:10 +0800

    Fixed all errors in calculator.c

--- calculator.c
+++ calculator.c
@@ -33,12 +33,12 @@ void getFiles()
         {
             if (strstr(dir-> d_name, ".usp"))
             {
-                files[count] = dir-> d_name; // might have to use strdup(dir->dirname)
+                files[count] = strdup(dir-> d_name); // might have to use strdup(dir->dirname)
                 // pipe error check
                 if (pipe(file_descripter + 2 * count) < 0)
                 {
                     perror("pipe");
-                    free(files[count]); // Maybe dont need to free
+                    free(files[count]);
                     exit(1);
                 }
                 count++;
@@ -46,47 +46,6 @@ void getFiles()
         }
         // close current directory
         closedir(directory);
-    }
-}
-
-// Function that creates parent and child processes and passes the files from 
-// parent to child. Child processes will run calculation function here.
-void create_child_processes()
-{
-    // for each parent process, create a child for it
-    for (int i = 0; i < count; i++)
-    {
-        pid_t pid = fork();
-        // fork error checkin
-        if (pid < 0)
-        {
-            perror("forking has failed");
-            free(files[i]);
-            exit(1);
-        }
-        
-        if (pid == 0)
-        {
-            // for each of the child processes, 
-            for (int c = 0; c < 2 * count; c++)
-            {
-                if (c != 2 * i + 1) // maybe c != 2 * i && c != 2 * i + 1
-                {
-                    close(file_descripter[c]);
-                }
-            }
-            // open file, using open() and O_RDONLY because cannot use fopen() and "r"
-            int file = open(files[i], O_RDONLY); // O_RDONLY is the read only for open()
-            // do calculation function here
-            close(file);
-            close(file_descripter[2 * i + 1]);
-            free(files[i]);
-            exit(0);
-        }
-        else
-        {
-            close(file_descripter[2 * i + 1]);
-        }
     }
 }
 
@@ -139,34 +98,69 @@ void calculator(int file, int i)
     // Do the calculation
     switch(math_operation[0])
     {
-        if (math_operation[0] == '+')
-        {
+        case '+':
             _result = num1 + num2;
-        }
-        else if (math_operation[0] == '-')
-        {
+            break;
+        case '-':
             _result = num1 - num2;
-        }
-        else if (math_operation[0] == '*')
-        {
-            _result = num1 * num2;
-        }
-        else if (math_operation[0] == '/')
-        {
+            break;
+        case '/':
             _result = num1 / num2;
-        }
-        else // error check for math_operation
-        {
-            perror("Math Operation Encountered is not Available");
-            free(files[i]);
-            exit(1);
-        }
+            break;
+        case '*':
+            _result = num1 * num2;
+            break;
+        default:
+        write(2, "Math Operation Not Available\n", 29);
+        free(files[i]);
+        exit(1);
     }
     sprintf(result, "%d", _result);
 
     // writing results to the pipe
     write(file_descripter[2 * i + 1], result, strlen(result) + 1);
     write(file_descripter[2 * i + 1], "\n", 1);
+}
+
+// Function that creates parent and child processes and passes the files from 
+// parent to child. Child processes will run calculation function here.
+void create_child_processes()
+{
+    // for each parent process, create a child for it
+    for (int i = 0; i < count; i++)
+    {
+        pid_t pid = fork();
+        // fork error checkin
+        if (pid < 0)
+        {
+            perror("forking has failed");
+            free(files[i]);
+            exit(1);
+        }
+        
+        if (pid == 0)
+        {
+            // for each of the child processes, 
+            for (int c = 0; c < 2 * count; c++)
+            {
+                if (c != 2 * i + 1) // maybe c != 2 * i && c != 2 * i + 1
+                {
+                    close(file_descripter[c]);
+                }
+            }
+            // open file, using open() and O_RDONLY because cannot use fopen() and "r"
+            int file = open(files[i], O_RDONLY); // O_RDONLY is the read only for open()
+            calculator(file, i);
+            close(file);
+            close(file_descripter[2 * i + 1]);
+            free(files[i]);
+            exit(0);
+        }
+        else
+        {
+            close(file_descripter[2 * i + 1]);
+        }
+    }
 }
 
 // Function that waits for child proccess to finish then gets the calculated results
@@ -195,7 +189,7 @@ void get_results()
         write(write_results, "\n", 1);
 
         // close pipe
-        close(file_descripter[2 * 1]);
+        close(file_descripter[2 * i]);
     }
     // close result.txt file
     close(write_results);
